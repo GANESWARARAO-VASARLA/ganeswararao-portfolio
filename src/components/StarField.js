60e405a00b0ac4acae5e93873034c397747df719
GANESWARARAO-VASARLA <ganeswararao.vasarla.44@gmail.com>
2025-01-14 14:31:42 +0530

    update

--- src/components/StarField.js
+++ src/components/StarField.js
@@ -7,8 +7,7 @@ const StarField = () => {
     const STAR_SIZE = 4;
     const STAR_MIN_SCALE = 0.2;
     const OVERFLOW_THRESHOLD = 50;
-    const STAR_COUNT = (window.innerWidth + window.innerHeight) / 10;
-  
+    const STAR_COUNT = (window.innerWidth + window.innerHeight) / 20;
     const canvas = document.querySelector("canvas"),
       context = canvas.getContext("2d");
   
